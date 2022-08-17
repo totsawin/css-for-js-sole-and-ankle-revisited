7b8509df46d78e4eaec0c9aee50f262657018677
totsawin <i_amnothere@live.com>
2022-08-17 13:18:52 +0700

    Exercise 3: Tweaks main view to align as column in tablet size and below

--- src/components/ShoeIndex/ShoeIndex.js
+++ src/components/ShoeIndex/ShoeIndex.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
-import { WEIGHTS } from '../../constants';
+import { WEIGHTS, QUERIES } from '../../constants';
 
 import Breadcrumbs from '../Breadcrumbs';
 import Select from '../Select';
@@ -31,12 +31,12 @@ const ShoeIndex = ({ sortId, setSortId }) => {
         <Breadcrumbs>
           <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
           <Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale/shoes">
-            Shoes
-          </Breadcrumbs.Crumb>
+          <Breadcrumbs.Crumb href="/sale/shoes">Shoes</Breadcrumbs.Crumb>
         </Breadcrumbs>
-        <Spacer size={42} />
-        <ShoeSidebar />
+        <DesktopSidebar>
+          <Spacer size={42} />
+          <ShoeSidebar />
+        </DesktopSidebar>
       </LeftColumn>
     </Wrapper>
   );
@@ -47,10 +47,17 @@ const Wrapper = styled.div`
   flex-direction: row-reverse;
   align-items: baseline;
   gap: 32px;
+  @media ${QUERIES.tabletAndBelow} {
+    flex-direction: column-reverse;
+    gap: 8px
+  }
 `;
 
 const LeftColumn = styled.div`
   flex-basis: 248px;
+  @media ${QUERIES.tabletAndBelow} {
+    flex-basis: auto;
+  }
 `;
 
 const MainColumn = styled.div`
@@ -66,6 +73,13 @@ const Header = styled.header`
 const Title = styled.h2`
   font-size: 1.5rem;
   font-weight: ${WEIGHTS.medium};
+`;
+
+const DesktopSidebar = styled.div`
+  display: block;
+  @media ${QUERIES.tabletAndBelow} {
+    display: none;
+  }
 `;
 
 export default ShoeIndex;
