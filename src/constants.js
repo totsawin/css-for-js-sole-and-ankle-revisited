1df60c383734d395505555f5b09ae8577aaadb18
totsawin <i_amnothere@live.com>
2022-08-17 09:14:28 +0700

    Exercise 1: set up breakpoints with deskto-first approach

--- src/constants.js
+++ src/constants.js
@@ -16,3 +16,15 @@ export const WEIGHTS = {
   medium: 600,
   bold: 800,
 };
+
+export const BREAKPOINTS = {
+  phoneMax: 600,
+  tabletMax: 950,
+  laptopMax: 1300,
+}
+
+export const QUERIES = {
+  'phoneAndBelow': `(max-width: ${BREAKPOINTS.phoneMax / 16}rem)`,
+  'tabletAndBelow': `(max-width: ${BREAKPOINTS.tabletMax / 16}rem)`,
+  'laptopAndBelow': `(max-width: ${BREAKPOINTS.laptopMax / 16}rem)`,
+}
